81c6ac940012246b0500677f0d3374d8db3e0f24
Allwyen <allwyen27@gmail.com>
2019-08-29 09:56:52 +0530

    updated

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ var app = new Express();
 app.use(Express.static(__dirname+'/dist/LibraryApp-angular'));
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist'))
+    res.sendFile(path.join(__dirname+'/dist/LibraryApp-angular/index.html'));
+});
+
+app.listen(process.env.PORT || 4455,()=>{
+    console.log("Server running on PORT:4455");
 });
